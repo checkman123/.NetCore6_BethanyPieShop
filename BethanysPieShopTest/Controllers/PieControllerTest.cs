5bc092739904b29db3932caf2be7a674ae899894
Nez <s.sirijirakarn@wsu.edu>
2022-11-21 09:23:22 -0800

    add test

---
+++ BethanysPieShopTest/Controllers/PieControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using BethanysPieShop.Controllers;
+using BethanysPieShop.ViewModels;
+using BethanysPieShopTest.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopTest.Controllers
+{
+    public class PieControllerTest
+    {
+
+        //Need Fact for it to be a testing method.
+        //Method name should be descripton of what we are testing
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            //Arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //Act
+            var result = pieController.List("");
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>
+                (viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
